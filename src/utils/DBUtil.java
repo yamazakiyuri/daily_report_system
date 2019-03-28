83a4e1a5d462ffc422594fa029e7cf26a8f59954
yuri.yamazaki <u101663g@yahoo.co.jp>
2019-03-28 13:38:52 +0900

    Add Employee class

--- src/utils/DBUtil.java
+++ src/utils/DBUtil.java
@@ -5,7 +5,7 @@ import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
 
 public class DBUtil {
-    private static final String PERSISTENCE_UNIT_NAME = "daily_report_sistem";
+    private static final String PERSISTENCE_UNIT_NAME = "daily_report_system";
     private static EntityManagerFactory emf;
 
     public static EntityManager createEntityManager() {
